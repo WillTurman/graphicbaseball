f277a60369a77ed3bc6b0307e0af83ac51702ee7
Will Turman <turmanw@gmail.com>
2012-11-20 18:39:19 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graphicbaseball::Application.config.secret_token = 'eadcc2bfd583f8021c00e4f96d6cf1eea8a95b9b57b8963385d6f3b9dd2ddc1cece6e002870eb83f3fa77b25c286ad76efa8cf58291f095acc00b130e40675cc'
